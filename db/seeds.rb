170ad096cbdf3e864bb4b722b940dca0ddd8bf22
rishithaw <rishithaw1@gmail.com>
2025-11-25 08:04:44 -0600

    Updated Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,40 @@
-if Rails.env.development?
-  AdminUser.where(email: "admin@example.com").first_or_initialize.tap do |admin|
-    admin.password = "password"
-    admin.password_confirmation = "password"
-    admin.save!
-  end
+# if Rails.env.development?
+#   AdminUser.where(email: "admin@example.com").first_or_initialize.tap do |admin|
+#     admin.password = "password"
+#     admin.password_confirmation = "password"
+#     admin.save!
+#   end
+# end
+
+# PageContent.find_or_create_by(name: "about")
+# PageContent.find_or_create_by(name: "contact")
+
+require "faker"
+
+puts "Clearing old data..."
+Product.destroy_all
+Category.destroy_all
+
+puts "Creating categories..."
+categories = [
+  "Courses",
+  "Templates",
+  "Software Tools",
+  "Digital Assets"
+].map do |name|
+  Category.create!(name: name)
 end
 
-PageContent.find_or_create_by(name: "about")
-PageContent.find_or_create_by(name: "contact")
+puts "Creating products..."
+100.times do
+  category = categories.sample
+
+  Product.create!(
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.paragraph(sentence_count: 5),
+    base_price: Faker::Commerce.price(range: 10..200),
+    category: category
+  )
+end
+
+puts "Done! Seeded #{Category.count} categories and #{Product.count} products."
